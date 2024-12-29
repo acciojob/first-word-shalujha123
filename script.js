773ff1e93814ccb2ab35c9012951c0095907e37c
Shalu <88194182+shalujha123@users.noreply.github.com>
2024-12-29 22:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function firstWord(s) {
   // your code here
-	let arr = s.split(" ")
-	return arr[0]
+	s = s.trim()
+	let spaceIndex = s.indexOf(" ")
+	if(spaceIndex === -1){
+		return s
+	}
+	return s.substring(0,spaceIndex)
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
